892235520dbef9a9597c4384509695dcaa1e1f85
Law WN <lawwnc@gmail.com>
2018-06-05 18:16:06 +0900

    added is_positive param through javascript

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -23,6 +23,7 @@ class FeelingsController < ApplicationController
   def create
     @feeling = Feeling.new(feeling_params)
     @feeling.user = current_user
+    binding.pry
     if @feeling.save
       redirect_to confirmation_feeling_path(@feeling)
     else
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,12 @@ piggy_button.addEventListener("click", (event) => {
   const baku_form = document.getElementById("baku-form");
   const piggy_form = document.getElementById("piggy-form");
   const baku_button = document.getElementById("baku-button");
+  const hidden_field = document.getElementById("is_positive");
   baku_form.classList.add("hidden");
   piggy_form.classList.remove("hidden");
   baku_button.classList.remove("hidden");
   piggy_button.classList.add("hidden");
+  hidden_field.value = true;
 });
 
 const baku_button = document.getElementById("baku-button");
@@ -16,8 +18,10 @@ baku_button.addEventListener("click", (event) => {
   const baku_form = document.getElementById("baku-form");
   const piggy_form = document.getElementById("piggy-form");
   const piggy_button = document.getElementById("piggy-button");
+  const hidden_field = document.getElementById("is_positive");
   piggy_form.classList.add("hidden");
   baku_form.classList.remove("hidden");
   piggy_button.classList.remove("hidden");
   baku_button.classList.add("hidden");
+  hidden_field.value = false;
 });
