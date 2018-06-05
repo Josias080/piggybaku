4c5e12e2222cfb184206009bac415c2b74207f32
Tomomi Shiba <follwthedot@gmail.com>
2018-06-05 22:39:36 +0900

    toggle animal images as well

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,29 @@
 import "bootstrap";
 
 const piggy_button = document.getElementById("piggy-button");
+const baku_button = document.getElementById("baku-button");
+const baku_form = document.getElementById("baku-form");
+const piggy_form = document.getElementById("piggy-form");
+const baku_img = document.getElementById("baku-img");
+const hidden_field = document.getElementById("is_positive");
+const piggy_img = document.getElementById("piggy-img");
+
 piggy_button.addEventListener("click", (event) => {
-  const baku_form = document.getElementById("baku-form");
-  const piggy_form = document.getElementById("piggy-form");
-  const baku_button = document.getElementById("baku-button");
-  const hidden_field = document.getElementById("is_positive");
   baku_form.classList.add("hidden");
   piggy_form.classList.remove("hidden");
   baku_button.classList.remove("hidden");
   piggy_button.classList.add("hidden");
+  baku_img.classList.add("hidden");
+  piggy_img.classList.remove("hidden");
   hidden_field.value = true;
 });
 
-const baku_button = document.getElementById("baku-button");
 baku_button.addEventListener("click", (event) => {
-  const baku_form = document.getElementById("baku-form");
-  const piggy_form = document.getElementById("piggy-form");
-  const piggy_button = document.getElementById("piggy-button");
-  const hidden_field = document.getElementById("is_positive");
   piggy_form.classList.add("hidden");
   baku_form.classList.remove("hidden");
   piggy_button.classList.remove("hidden");
   baku_button.classList.add("hidden");
+  piggy_img.classList.add("hidden");
+  baku_img.classList.remove("hidden");
   hidden_field.value = false;
 });
