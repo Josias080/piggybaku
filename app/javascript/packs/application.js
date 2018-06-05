7e7d52680365056ce0f13020fb02b808793a75bb
Law WN <lawwnc@gmail.com>
2018-06-05 17:06:14 +0900

    toggling forms done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,22 @@ import "bootstrap";
 
 const piggy_button = document.getElementById("piggy-button");
 piggy_button.addEventListener("click", (event) => {
-  @type === true
+  const baku_form = document.getElementById("baku-form");
+  const piggy_form = document.getElementById("piggy-form");
+  const baku_button = document.getElementById("baku-button");
+  baku_form.classList.add("hidden");
+  piggy_form.classList.remove("hidden");
+  baku_button.classList.remove("hidden");
+  piggy_button.classList.add("hidden");
 });
 
-  // const baku_form = document.getElementById("baku_form");
-  // const piggy_form = document.getElementById("piggy_form");
-  // baku_form.style.display = "none";
+const baku_button = document.getElementById("baku-button");
+baku_button.addEventListener("click", (event) => {
+  const baku_form = document.getElementById("baku-form");
+  const piggy_form = document.getElementById("piggy-form");
+  const piggy_button = document.getElementById("piggy-button");
+  piggy_form.classList.add("hidden");
+  baku_form.classList.remove("hidden");
+  piggy_button.classList.remove("hidden");
+  baku_button.classList.add("hidden");
+});
