6b6fc16e8c4dbf9ed10a3424dedb588f78216a74
Law WN <lawwnc@gmail.com>
2018-06-07 18:28:12 +0900

    corrected whitespace in api key for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_06_07_060543) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_06_07_060543) do
     t.integer "flowers", default: 0
     t.index ["user_id"], name: "index_feelings_on_user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
