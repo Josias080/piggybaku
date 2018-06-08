7971d1e7132d8a53f9a11dcfb510bf310d6bf323
Josias Geoffroy <geoffroyjosias@mac.com>
2018-06-08 11:01:51 +0900

    push the diff schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_06_07_060543) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,12 +23,12 @@ ActiveRecord::Schema.define(version: 2018_06_07_060543) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_donated", default: false, null: false
     t.boolean "is_buried", default: false, null: false
     t.integer "flowers", default: 0
+    t.boolean "is_donated", default: false, null: false
     t.index ["user_id"], name: "index_feelings_on_user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
