e988fa631d7b7151ec7f8ab79f245a41edddf1a9
Tomomi Shiba <follwthedot@gmail.com>
2018-06-10 00:05:26 +0900

    animation on feelingnew page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,4 +32,5 @@ baku_button.addEventListener("click", (event) => {
 
 $(".toggle-animal-btn-container").click(function () {
   $(".fa-angle-double-left").toggleClass("arrow-pig");
+  $(".animal-container").toggleClass("rollIn");
 });
